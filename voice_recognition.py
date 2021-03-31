3e0e5b2617e28802fecfbc892a47e5531b8292b0
Advitama <rizkiasmoro@gmail.com>
2021-03-31 15:57:10 -0700

    voice recognition

---
+++ voice_recognition.py
@@ -0,0 +1,15 @@
+'''
+Voice Recognition
+'''
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak Anything now:")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said : {}".format(text))
+    except:
+        print("Sorry could not recognize what you said")
