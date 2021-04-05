95977cc5baeb011618dee58a93415c0357fb06f7
Advitama <rizkiasmoro@gmail.com>
2021-04-05 14:38:23 +0700

    voice translation

---
+++ voice_translate.py
@@ -0,0 +1,17 @@
+from googletrans import Translator
+import speech_recognition as voice
+
+x = voice.Recognizer()
+y = Translator()
+
+with voice.Microphone() as source:
+    x.adjust_for_ambient_noise(source)
+    print("Speak Anything now:")
+    audio = x.listen(source)
+    try:
+        text = x.recognize_google(audio)
+        result = y.translate(text, dest='jw')
+ #       print("You said : {}".format(text))
+        print (result.text)        
+    except:
+        print("Sorry could not recognize what you said")
