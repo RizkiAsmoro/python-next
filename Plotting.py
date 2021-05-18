67e83945c7a7862961c17ea0b2b3896083b6079a
Advitama <rizkiasmoro@gmail.com>
2021-05-18 18:00:42 +0700

    plotting with and without line

--- Plotting.py
+++ Plotting.py
@@ -6,8 +6,16 @@ Plotting - Basic Pyplot
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoint = np.array([0,6])
-ypoint = np.array([0,50])
+xpoint = np.array([0,5,7,12,15,20])
+ypoint = np.array([0,7,6,13,10,17])
 
 plt.plot(xpoint, ypoint)
+plt.show()
+
+
+#plotting without line
+xp = np.array([0,5,7,12,15,20])
+yp = np.array([0,7,6,13,10,17])
+
+plt.plot(xp, yp,'o')
 plt.show()
