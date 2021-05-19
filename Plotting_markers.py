669b92dc4973aec523a0eecfa173ede70c8806a7
Advitama <rizkiasmoro@gmail.com>
2021-05-19 14:23:32 +0700

    plotting markers

---
+++ Plotting_markers.py
@@ -0,0 +1,36 @@
+'''
+Plotting Markers
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+yp = np.array([0,7,6,13,10,17])
+plt.plot(yp, marker ="*") 
+plt.show()
+'''
+Marker references :
+'o'	Circle	
+'*'	Star	
+'.'	Point	
+','	Pixel	
+'x'	X	
+'X'	X (filled)	
+'+'	Plus	
+'P'	Plus (filled)	
+'s'	Square	
+'D'	Diamond	
+'d'	Diamond (thin)	
+'p'	Pentagon	
+'H'	Hexagon	
+'h'	Hexagon	
+'v'	Triangle Down	
+'^'	Triangle Up	
+'<'	Triangle Left	
+'>'	Triangle Right	
+'1'	Tri Down	
+'2'	Tri Up	
+'3'	Tri Left	
+'4'	Tri Right	
+'|'	Vline	
+'_'	Hline
+'''
