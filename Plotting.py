7987ca48d3b82627ba257a84c0bdc50d16230a4a
Advitama <rizkiasmoro@gmail.com>
2021-05-19 15:18:49 +0700

    plotting 2 lines

--- Plotting.py
+++ Plotting.py
@@ -26,3 +26,12 @@ xa = np.array([0,7,6,13,10,17])
 plt.plot(xa)
 plt.show()
 
+#plotting 2 lines
+x1 = np.array([0,5,7,12,15,20])
+x2 = np.array([0,7,6,13,10,17])
+
+plt.plot(x1)
+plt.plot(x2)
+plt.show()
+
+
